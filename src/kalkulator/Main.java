f41fd86f268d01d0bf3545040949432106fdae0b
slavko024 <icex024@gmail.com>
2021-11-03 15:25:10 +0100

    Empty line check

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -7,9 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scr = new Scanner(System.in);
-		String komanda;
+		String komanda="";
 		do {
 			komanda = scr.nextLine();
+			if (komanda.isEmpty()) {
+				continue;
+			}
 		}while(!komanda.equals("End"));
 	}
 
