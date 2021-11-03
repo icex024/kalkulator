8842348b15b4a9c3e35f16e48410bf02c2bb5803
slavko024 <icex024@gmail.com>
2021-11-03 21:16:42 +0100

    Implement addition

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -9,11 +9,26 @@ public class Main {
 		Scanner scr = new Scanner(System.in);
 		String komanda="";
 		do {
+			String[] kom=komanda.split(" ");
 			komanda = scr.nextLine();
 			if (komanda.isEmpty()) {
 				continue;
 			}
-			String[] kom=komanda.split(" ");
+			
+			
+			int x=Integer.parseInt(kom[0]);
+			int y=Integer.parseInt(kom[2]);
+			int z=0;
+			switch (kom[1]) {
+			case "+":
+				z=x+y;
+				break;
+			default:
+				System.out.println("Operand nije podrzan");
+				continue;
+				
+			}
+			System.out.println("Rezultat "+z);
 			if (kom.length!=3) {
 				continue;
 			}
