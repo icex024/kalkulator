6e18ab61e9e87667645cef7606cd252f80ded599
slavko024 <icex024@gmail.com>
2021-11-03 21:31:51 +0100

    Added function for adding

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -2,6 +2,7 @@ package kalkulator;
 
 import java.util.Scanner;
 
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,30 +10,32 @@ public class Main {
 		Scanner scr = new Scanner(System.in);
 		String komanda="";
 		do {
-			String[] kom=komanda.split(" ");
 			komanda = scr.nextLine();
 			if (komanda.isEmpty()) {
 				continue;
 			}
 			
-			
+			String[] kom=komanda.split(" ");
 			int x=Integer.parseInt(kom[0]);
 			int y=Integer.parseInt(kom[2]);
 			int z=0;
 			switch (kom[1]) {
 			case "+":
-				z=x+y;
+				z=addition(x,y);
 				break;
 			default:
 				System.out.println("Operand nije podrzan");
 				continue;
-				
 			}
 			System.out.println("Rezultat "+z);
 			if (kom.length!=3) {
 				continue;
 			}
 		}while(!komanda.equals("End"));
+		scr.close();
+	}
+	public static int addition(int a,int b) {
+		return a+b;
 	}
 
 }
