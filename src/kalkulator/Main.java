b734c9902490322bdd00823338499b2ff3f7d253
slavko024 <icex024@gmail.com>
2021-11-03 15:20:45 +0100

    Initaial read line

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,16 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scr = new Scanner(System.in);
+		String komanda;
+		do {
+			komanda = scr.nextLine();
+		}while(!komanda.equals("End"));
 	}
 
 }
